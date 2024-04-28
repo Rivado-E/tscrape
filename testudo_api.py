97eef6a2c747341f595f11dee248b320b20abd16
Rivado Edah <redah@umd.edu>
2024-04-28 13:24:25 -0400

    added wrapper

--- testudo_api.py
+++ testudo_api.py
@@ -1,15 +1,12 @@
 import pandas as pd
-import ast
 from datetime import datetime
 
 df = pd.read_csv("sub_sections.csv")
 
-df = pd.read_csv("sections.csv")
-
 
 def query_rows_by_start_time_gen(start_time, data=df):
     start_time_dt = datetime.strptime(start_time, "%I:%M%p").time()
-    return data[data['start_time'].apply(lambda x: datetime.strptime(x, "%I:%M%p").time() == start_time_dt)]
+    return data[data['start_time'].apply(lambda x: datetime.strptime(x, "%I:%M%p").time() == start_time_dt)].to_dict(orient="records")
 
 
 def verify_conflict_gen(start_time, end_time, data=df):
@@ -17,22 +14,22 @@ def verify_conflict_gen(start_time, end_time, data=df):
     end_time_dt = datetime.strptime(end_time, "%I:%M%p").time()
     first = data[data['start_time'].apply(lambda x: datetime.strptime(x, "%I:%M%p").time() <= start_time_dt)]
     second = first[first['end_time'].apply(lambda x: datetime.strptime(x, "%I:%M%p").time() >= end_time_dt)]
-    return second
+    return second.to_dict(orient="records")
 
 
 def subset_school(school, data=df):
-    return data[data['course_code'].apply(lambda x: school in x)] 
+    return data[data['course_code'].apply(lambda x: school in x)]
 
 
 def query_rows_by_start_time(start_time, school, data=df):
     start_time_dt = datetime.strptime(start_time, "%I:%M%p").time()
-    return data[data['start_time'].apply(lambda x: datetime.strptime(x, "%I:%M%p").time() == start_time_dt)]
+    return data[data['start_time'].apply(lambda x: datetime.strptime(x, "%I:%M%p").time() == start_time_dt)].to_dict(orient="records")
 
 
 def verify_conflict(start_time, end_time, school, data=df):
     data = subset_school(school, data)
     start_time_dt = datetime.strptime(start_time, "%I:%M%p").time()
     end_time_dt = datetime.strptime(end_time, "%I:%M%p").time()
-    first = data[data['start_time'].apply(lambda x: datetime.strptime(x, "%I:%M%p").time() <= start_time_dt)]
-    second = first[first['end_time'].apply(lambda x: datetime.strptime(x, "%I:%M%p").time() >= end_time_dt)]
-    return second
+    first = data[data["start_time"].apply(lambda x: datetime.strptime(x, "%I:%M%p").time() < end_time_dt)]
+    first = first[first["end_time"].apply(lambda x: datetime.strptime(x, "%I:%M%p").time() > start_time_dt)]
+    return first.to_dict(orient="records")
